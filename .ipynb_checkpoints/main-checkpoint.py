f35b3e0d6f7498efb62bdf1bbb9e756a82b7f8fe
dbbenson <dylanbenson@duck.com>
2023-03-02 11:21:08 -0600

    Commit Entire directory

---
+++ .ipynb_checkpoints/main-checkpoint.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+df = pd.read_csv('DMC.csv')
+print(df)
+
+
+
+
--- main.py
+++
@@ -1,4 +0,0 @@
-import csv
-
-with open('Coffee_domestic_consumption.csv') as csv_file:
-    csv_reader = csv.reader(csv_file)
